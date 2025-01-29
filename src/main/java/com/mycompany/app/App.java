192c684d3c6d954d55e5e2ee822f5a5267ea8bee
parveenshaiksab <a00325377@student.tus.ie>
2025-01-29 12:21:31 +0000

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println(MESSAGE);
         system.out.println("hello");
+        system.out.println("hello world");
     }
 
     public String getMessage() {
